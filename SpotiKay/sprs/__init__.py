000e61138ffb366f84c6c0513d19ad5344a724cb
Ekaterina Romanovskaya <64712690+KathyRoma@users.noreply.github.com>
2020-07-30 16:43:50 -0700

    Updated according to front/backend request

--- SpotiKay/sprs/__init__.py
+++ SpotiKay/sprs/__init__.py
@@ -1,37 +1,30 @@
 from os import getenv
 from flask import Flask, render_template, request
-
 from sqlalchemy_utils import database_exists
 from flask_sqlalchemy import SQLAlchemy
 # from sqlalchemy.ext.declarative import declarative_base
 # from sqlalchemy.orm import sessionmaker
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
 DB = SQLAlchemy(app)
-
 from .models import Song
 from .predict import predict_best_songs
 from .spotify import add_song
-
-
 def create_app():
     """ Creates and configures a flask app"""
-
     if not database_exists(getenv('DATABASE_URL')):
         DB.drop_all()
         DB.create_all()    
     return app
 
+#Updated by Hernan 
 @app.route('/')
 def root():
     return render_template('base.html', title='Home')
 
 @app.route('/compare', methods=['POST'])
 def compare(message=''):
-    song, artist = ([request.values['song_name'],
-                            request.values['artist_name']])
-    suggestions = predict_best_songs(song, artist)
-
+    song = request.values['track_id']
+    suggestions = predict_best_songs(song)
     return suggestions
--- SpotiKay/sprs/predict.py
+++ SpotiKay/sprs/predict.py
@@ -6,58 +6,39 @@ from pandas import DataFrame
 import sqlite3
 from sklearn.neighbors import NearestNeighbors
 import json
-
-
-def predict_best_songs(song_name, artist_name):
+def predict_best_songs(track_id):
     """Determines and returns which user is more likely to say a given Tweet"""
-
-    #Store track id to variable
-    track_id = track_id_for_artist_title(artist_name, song_name)
-
     #This will give us a list of features necessary for prediction
     #Code by Ekaterina & Hernan
     song_to_predict = get_features(track_id)
-
     #Add song to existing DB
     #Code by Hernan
     add_song(song_to_predict)
-
     #This K Means model will give us a list of recommended songs
     #Code by Josh
     def predicto(track_id):
-
         # Instantiate and fit knn to the correct columns
         knn = NearestNeighbors(n_neighbors=20)
         knn.fit(df[df.columns[5:]])
-
         obs = df.index[df['id'] == track_id]
         series = df.iloc[obs, 5:].to_numpy()
-
         neighbors = knn.kneighbors(series)
         new_obs = neighbors[1][0][6:20]
         return list(df.loc[new_obs, 'id'])
-    
     #Converting the DB to a DF to run a K Means model through
     conn = sqlite3.connect('sprs/spotitry_songs.db')
     curs = conn.cursor()
-
     SQL_Query = pd.read_sql_query(''' SELECT * from song ''',conn)
-
     df = pd.DataFrame(SQL_Query, columns=['id','name','energy',
                                       'liveness','danceability','instrumentalness','loudness',
                                       'speechiness','valence','tempo'])
-    
     track_list = predicto(track_id)
-    
     #Here we'll turn our list of track ids into song names
     #Code by Ekaterina & Hernan
+    #Re-written by Hernan to return json with feature list
     suggestions = get_features(track_list[0])
-
     column_names = ['track_id', 'name', 'acousticness', 'danceability', 'duration_ms', 'energy', 'instrumentalness',
                     'liveness', 'loudness', 'speechiness', 'tempo', 'valence']
-
     final = pd.DataFrame([suggestions], columns=column_names)
-
     result = final.to_json()
-
     return result
